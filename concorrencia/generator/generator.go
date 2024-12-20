902b69128606639cfdbf5508ea3c769b74c4a4c6
Bruno Pueyo do Amaral <pueyonit@gmail.com>
2024-12-20 16:15:41 -0300

    novas funcionlidades

--- concorrencia/generator/generator.go
+++ concorrencia/generator/generator.go
@@ -2,41 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"regexp"
+
+	titulo "github.com/07669751770/curso-golang/titulo"
 )
 
-func titulo(urls []string) <-chan string {
-	c := make(chan string)
-	for _, url := range urls {
-		go func(url string) {
-			resp, _ := http.Get(url)
-			html, _ := io.ReadAll(resp.Body)
-			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
-			re := r.FindStringSubmatch(string(html))
-			if len(re) >= 2 {
-				c <- r.FindStringSubmatch(string(html))[1]
-			} else {
-				c <- "Titulo ausente"
-			}
-
-		}(url)
-	}
-	return c
-}
-
 func main() {
-	titulos := []string{"https://www.google.com", "https://www.facebook.com", "https://www.youtube.com", "https://www.amazon.com", "https://www.wikipedia.org"}
-	titulos2 := []string{"https://www.reddit.com", "https://www.twitter.com", "https://www.linkedin.com", "https://www.instagram.com", "https://www.netflix.com"}
+	titulos := []string{"https://www.google.com", "https://www.facebook.com"}
+	titulos2 := []string{"https://www.reddit.com", "https://www.twitter.com"}
 
-	t1 := titulo(titulos)
-	t2 := titulo(titulos2)
+	t1 := titulo.Titulo(titulos)
+	t2 := titulo.Titulo(titulos2)
 
 	fmt.Println("Primeiros:", <-t1, "|", <-t2)
 	fmt.Println("Segundos:", <-t1, "|", <-t2)
-	fmt.Println("Terceiros:", <-t1, "|", <-t2)
-	fmt.Println("Quartos:", <-t1, "|", <-t2)
-	fmt.Println("Quintos:", <-t1, "|", <-t2)
 
 }
---
+++ concorrencia/generator/go.mod
@@ -0,0 +1,5 @@
+module github.com/07669751770/curso-golang/generator
+
+go 1.23.3
+
+require github.com/07669751770/curso-golang v0.0.0-20241220185720-80d31d8cd589 // indirect
